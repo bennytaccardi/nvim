186418e6793cb2e3f4c2742bbffda9b3a598dc7b
BennyTaccardi <benny.taccardi@gmail.com>
2024-06-21 00:07:02 +0200

    config improvement

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,6 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Map Shift+Cmd+F to Telescope's live_grep function
-map("n", "<D-F>", "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
-map("n", "<D-s>", ":w<CR>", opts)
-map("i", "<D-s>", "<Esc>:w<CR>a", opts)
+map("n", "<C-l>", "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
+map("n", "<C-s>", ":w<CR>", opts)
+map("i", "<C-s>", "<Esc>:w<CR>a", opts)
