627268e05ce4809fc81fd1b3bb981cc1d41a48c0
BennyTaccardi <benny.taccardi@gmail.com>
2024-06-21 11:35:24 +0200

    config improvement

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map("n", "<C-l>", "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true, silent = true })
+map("n", "<C-p>", "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true, silent = true })
 map("n", "<C-i>", "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true, silent = true })
 map("n", "<C-s>", ":w<CR>", opts)
 map("i", "<C-s>", "<Esc>:w<CR>a", opts)
